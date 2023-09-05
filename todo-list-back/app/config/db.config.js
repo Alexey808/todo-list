a935199b4adfaa064691a3f92f65a4fd90396436
alexey <alexey808sv@gmail.com>
2023-09-06 00:24:57 +0400

    [back] add dockerfile

--- todo-list-back/app/config/db.config.js
+++ todo-list-back/app/config/db.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  url: 'mongodb://127.0.0.1:27017/todo_db'
+  // "mongodb" вместо "127.0.0.1:27017" это название сервиса в docker-compose.yml
+  url: 'mongodb://mongodb/todo_db'
 };
